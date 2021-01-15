a6d0d3a936a365ee06b7bee0b5fe42c138754a38
unknown <hunny.rahi55@gmail.com>
2021-01-15 12:16:27 -0800

    making more responsive haha

--- src/component/auth/login/login.js
+++ src/component/auth/login/login.js
@@ -5,27 +5,13 @@ import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
 
 import Loader from "react-loader-spinner";
 import Footer from "../../footer/footer";
+import Navbar from "../../home/navbar/navbar";
 
 function Login() {
   return (
     <React.Fragment>
       <div class="main_hero">
-        <div class="top_header">
-          <div class="logo">
-            <img
-              src="https://www.abacustechnologies.com/wp-content/uploads/2020/05/580b57fcd9996e24bc43c529.png"
-              width="100"
-              alt="netflix_logo"
-            />
-          </div>
-          <div class="navbar">
-            <ul>
-              <li>
-                <a href="">Login</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Navbar />
 
         <div class="login_form">
           <h2>Sign in</h2>
--- src/component/auth/signup/signup.js
+++ src/component/auth/signup/signup.js
@@ -1,28 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Footer from "../../footer/footer";
+import Navbar from "../../home/navbar/navbar";
 import "./signup.css";
 
 function Signup() {
   return (
     <React.Fragment>
       <div class="main_hero">
-        <div class="top_header">
-          <div class="logo">
-            <img
-              src="https://www.abacustechnologies.com/wp-content/uploads/2020/05/580b57fcd9996e24bc43c529.png"
-              width="100"
-              alt="netflix_logo"
-            />
-          </div>
-          <div class="navbar">
-            <ul>
-              <li>
-                <a href="">Login</a>
-              </li>
-            </ul>
-          </div>
-        </div>
+        <Navbar />
 
         <div class="signup_form">
           <h2>Sign in</h2>
